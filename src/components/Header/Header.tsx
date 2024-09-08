a78248f0965e9b733389dde2fc2fe9de5110fa8e
Anna Yakubovich <yakubovichannie@gmail.com>
2024-09-09 01:26:44 +0300

    tailwindcss added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,54 +2,53 @@ import { FC } from "react";
 import { Link } from "react-router-dom";
 import { ReactSVG } from "react-svg";
 import { cart, logo, search } from "../../assets/img/assets";
-import "./Header.scss";
 
 const Header: FC = () => {
   return (
-    <header className="header">
-      <div className="container header__container">
-        <Link className="header__logo" to={'/'}>
+    <header className="pt-[69px] pb-[69px]">
+      <div className="flex items-center w-full max-w-[1636px] mx-auto pl-[20px] pr-[20px]">
+        <Link className="flex-shrink-0 mr-[150px]" to={'/'}>
           <img src={logo} alt="Organick" />
         </Link>
 
-        <nav className="header__nav nav">
-          <ul className="nav__list">
-            <li className="nav__item">
-              <Link className="nav__link" to={"/"}>Home</Link>
+        <nav className="mr-auto text-xl font-bold">
+          <ul className="flex items-center gap-10">
+            <li>
+              <Link to={"/"}>Home</Link>
             </li>
-            <li className="nav__item">
-              <Link className="nav__link" to={"/about"}>About</Link>
+            <li>
+              <Link to={"/about"}>About</Link>
             </li>
-            <li className="nav__item">
-              <select className="nav__link nav__select" id="">
+            <li>
+              <select id="pages">
                 <option value="">Pages</option>
               </select>
             </li>
-            <li className="nav__item">
-              <Link className="nav__link" to={"/shop"}>Shop</Link>
+            <li>
+              <Link to={"/shop"}>Shop</Link>
             </li>
-            <li className="nav__item">
-              <Link className="nav__link" to={"/portfolio"}>Projects</Link>
+            <li>
+              <Link to={"/portfolio"}>Projects</Link>
             </li>
-            <li className="nav__item">
-              <Link className="nav__link" to={"/blog"}>News</Link>
+            <li>
+              <Link to={"/blog"}>News</Link>
             </li>
           </ul>
         </nav>
 
-        <div className="header__search-bar search-bar">
-          <input className="search-bar__input" type="text" />
-          <button className="search-bar__button">
-            <ReactSVG src={search} />
+        <div className="relative mr-[18px] w-[376px] h-[66px]">
+          <input className="w-full h-full rounded-[36px] bg-[var(--clr-alabaster-light)]" type="text" />
+          <button className="absolute top-[5px] right-[6px] flex-shrink-0 flex items-center justify-center w-14 h-14 rounded-full bg-[var(--clr-acapulco)]">
+            <ReactSVG className="text-center" src={search} />
           </button>
         </div>
 
-        <div className="header__cart cart">
-          <div className="cart__logo">
+        <div className="flex items-center gap-3 pt-[5px] pr-[23px] pb-[5px] pl-[6px] border border-[var(--clr-alto-light)] rounded-[33px]">
+          <div className="flex-shrink-0 flex items-center justify-center w-14 h-14 rounded-full text-[0] bg-[var(--clr-blue-dianne)]">
             <ReactSVG src={cart} />
           </div>
-          <p className="cart__title">
-            Cart <span className="cart__quantity">(0)</span>
+          <p className="text-lg font-semibold">
+            Cart <span id="cart-quantity">(0)</span>
           </p>
         </div>
       </div>
